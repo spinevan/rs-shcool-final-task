62e8c3a4a729757c3ddcc08ba142ec748835df1b
IvanLaptop <sinitsyniv@gmail.com>
2021-12-02 14:24:44 +0300

    feat: tests detail VM ref

--- app/src/test/java/ru/sinitsyndev/rs_shcool_final_task/mainScreen/ui/MainViewModelTest.kt
+++ app/src/test/java/ru/sinitsyndev/rs_shcool_final_task/mainScreen/ui/MainViewModelTest.kt
@@ -1,31 +1,14 @@
 package ru.sinitsyndev.rs_shcool_final_task.mainScreen.ui
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Lifecycle
 import junit.framework.TestCase
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.job
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runBlockingTest
-import kotlinx.coroutines.test.setMain
 import org.junit.*
-import org.junit.Assert.assertThat
 import org.junit.runner.RunWith
 import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnitRunner
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import ru.sinitsyndev.rs_shcool_final_task.MainCoroutineRule
-import ru.sinitsyndev.rs_shcool_final_task.data.CoinCapRepositoryImpl
-import ru.sinitsyndev.rs_shcool_final_task.data.retrofit.CoinCapAPI
-import ru.sinitsyndev.rs_shcool_final_task.data.retrofit.CoinCapAPIRetrofitClient
 import ru.sinitsyndev.rs_shcool_final_task.mainScreen.domain.GetAssetsListUseCase
-import ru.sinitsyndev.rs_shcool_final_task.utils.COIN_CAP_API_BASE_URL
-
 
 @ExperimentalCoroutinesApi
 @RunWith(MockitoJUnitRunner::class )
